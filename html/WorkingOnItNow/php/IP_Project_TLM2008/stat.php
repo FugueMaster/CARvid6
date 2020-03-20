a46950c4d5631dbe1d32fcb98ee4c890173bee3c
ryccagrace <rycjia@gmail.com>
2020-03-20 14:22:12 +0800

    copy and paste into htdocs to run project

---
+++ html/WorkingOnItNow/php/IP_Project_TLM2008/stat.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'header.inc'?>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>TRINDS</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+  
+  <style>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+  
+  
+div.a {
+  line-height: 10;
+}
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+  
+  
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Moderna - v2.0.0
+  * Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+<body>
+
+   <header id="header" class="fixed-top ">
+	  <div class="container">
+
+	    <div class="logo float-left">
+            <h1 class="text-light"><a href="webpage.html"><span>TRINDS</span></a></h1>
+            <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+	    </div>
+	
+	      <nav class="nav-menu float-right d-none d-lg-block"> <ul>
+          <li class="drop-down"><a href="webpage.html">Home</a>
+		  <ul>
+		    <li><a href="about.html">About Us</a></li>
+          </ul>
+		  </li>
+		  <li><a href="map.html">Map</a></li>
+          <li class="drop-down"><a href="stat.html">Statistics</a>
+                <ul>
+                  <li><a href="#">Overall</a></li>
+                  <li><a href="#">Accidents</a></li>
+                  <li><a href="#">Breakdowns</a></li>
+                  <li><a href="#">Road Works</a></li>
+                  <li><a href="#">Heavy Traffic</a></li>
+                </ul>
+		  </li>		
+             
+          <li class="drop-down"><a href="#">Contact Us</a>
+		       <ul>
+				 <li><a href="faq.html">FAQ</a></li>
+				 <li><a href="entry.html">Submit Entry</a></li>
+               </ul>
+		  </li>	   
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+  
+  
+  <main id="main">
+  
+  
+  <!-- ======= Stats Section ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Statistics</h2>
+          <ol>
+            <li><a href="webpage.html">Home</a></li>
+			<li>Statistics</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+	
+	
+	
+	<!-- ======= Stats Section ======= -->
+    <section class="stats" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
+      <div class="container">
+	
+  
+  <table>
+  
+  <tr>
+    <th>Traffic Update</th>
+    <th>Date/Time</th>
+  </tr>
+  <tr>
+    <td>Heavy Traffic on PIE (towards Changi Airport) after BKE</td>
+    <td>04/03 16:27</td>
+  </tr>
+  <tr>
+    <td>Heavy Traffic on SLE (towards BKE) at Woodlands Ave 12 Exit.</td>
+    <td>04/03 16:53</td>
+  </tr>
+</table>
+
+  </main><!-- End #main -->
+</body>
+<?php include 'footer.inc'?>
+</html>	
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
