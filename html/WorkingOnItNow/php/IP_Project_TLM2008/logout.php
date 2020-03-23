055fb4a315779d9b2b2db1a1991c833baa53052b
ryccagrace <rycjia@gmail.com>
2020-03-23 12:34:46 +0800

    clean up and updated php files

---
+++ html/WorkingOnItNow/php/IP_Project_TLM2008/logout.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+session_destroy();
+header('Location:Login.php');
+exit;
+
+?>
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
