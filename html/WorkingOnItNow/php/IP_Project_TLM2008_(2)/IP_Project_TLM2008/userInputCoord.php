07353d3eba58cdbb9397338bc62fce66d5906305
Luna Neo <luna_neolq24@outlook.com>
2020-03-24 17:58:28 +0800

    update userInputCoord.php

--- html/WorkingOnItNow/php/IP_Project_TLM2008_(2)/IP_Project_TLM2008/userInputCoord.php
+++ html/WorkingOnItNow/php/IP_Project_TLM2008_(2)/IP_Project_TLM2008/userInputCoord.php
@@ -8,20 +8,40 @@
             <tr>
                 <td valign="top" style="width: 50%; height: 400px">
                     <p>
-                        <label>Latitude:</label>
-                        <input type="text" id="latitude" value="" />
-                    </p>
-                    <p>
                         <label>Longitude:</label>
                         <input type="text" id="longitude" value="" />
                     </p>
                     <p>
-                        <input type="text" id="propertyname1" value="date" />
-                        <input type="text" id="propertyvalue1" value="" />
+                        <label>Latitude:</label>
+                        <input type="text" id="latitude" value="" />
                     </p>
                     <p>
-                        <input type="text" id="propertyname2" value="foo" />
-                        <input type="text" id="propertyvalue2" value="" />
+                        <input type="text" id="iid" value="Issue No." />
+                        <input type="text" id="iid_value" value="" />
+                    </p>
+                    <p>
+                        <input type="text" id="timestamp" value="Time Reported" />
+                        <input type="text" id="timestamp_value" value="" />
+                    </p>
+                    <p>
+                        <input type="text" id="comments" value="Comments" />
+                        <input type="text" id="comments_value" value="" /> 
+                    </p>
+                    <p>
+                        <input type="text" id="type" value="Issue Type" />
+                        <input type="text" id="type_value" value="" />
+                    </p>
+                    <p>
+                        <input type="text" id="location" value="Location" />
+                        <input type="text" id="location_value" value="" />
+                    </p>
+                    <p>
+                        <input type="text" id="pid" value="Username" />
+                        <input type="number" id="pid_value" value="" />
+                    </p>
+                    <p>
+                        <input type="text" id="icon" value="Icon" />
+                        <input type="text" id="icon_value" value="" />
                     </p>
                     <p> 
                     <!-- starts here -->
@@ -50,8 +70,17 @@
                         lng: document.getElementById("longitude").value
                     }
                 ];
-                data[0][document.getElementById("propertyname1").value] = document.getElementById("propertyvalue1").value;
-                data[0][document.getElementById("propertyname2").value] = document.getElementById("propertyvalue2").value;
+                data[0][document.getElementById("iid").value] = document.getElementById("iid_value").value;
+                data[0][document.getElementById("timestamp").value] = document.getElementById("timestamp_value").value;
+                data[0][document.getElementById("comments").value] = document.getElementById("comments_value").value;
+                data[0][document.getElementById("type").value] = document.getElementById("type_value").value;
+                data[0][document.getElementById("location").value] = document.getElementById("location_value").value;
+                data[0][document.getElementById("pid").value] = document.getElementById("pid_value").value;
+                data[0][document.getElementById("icon").value] = document.getElementById("icon_value").value;
+
+
+
+
                 const dataGeoJSON = GeoJSON.parse(data, { Point: ["lat", "lng"] });
                 out = JSON.stringify(dataGeoJSON, null, 4);
                 document.getElementById("output").value = out;
