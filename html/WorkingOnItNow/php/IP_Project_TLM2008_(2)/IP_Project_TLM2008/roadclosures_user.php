ee91f4fd37a7dce04263638daea17d5a2f27b70c
ryccagrace <rycjia@gmail.com>
2020-03-24 20:45:46 +0800

    comments for stat php

--- html/WorkingOnItNow/php/IP_Project_TLM2008_(2)/IP_Project_TLM2008/roadclosures_user.php
+++ html/WorkingOnItNow/php/IP_Project_TLM2008_(2)/IP_Project_TLM2008/roadclosures_user.php
@@ -8,27 +8,29 @@ and open the template in the editor.
  <?php include 'header.inc'?>
 <link rel="stylesheet" href="assets/css/display_data.css">
 <?php
+//assign required variables for db connection link
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "ip_schema";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);//connection to database
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM enter_issue E, closure C where E.iid = C.iid";
-$result = $conn->query($sql);
+$sql = "SELECT * FROM enter_issue E, closure C where E.iid = C.iid";//for displaying of all road closures info for user in webpage
+$result = $conn->query($sql);//send query to mysql server
 
 if ($result->num_rows > 0) {
     echo "<h2><u><b>Road Closures</b></u></h2>";
     echo "<table><tr><th>IID</th><th>Time</th><th>Date</th><th>Comment</th><th>Location</th><th>Start Date(yyyy-mm-dd)</th><th>End Date(yyyy-mm-dd)</th></tr>";
     // output data of each row
+    /* fetch associative array */
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["iid"]. "</td><td>" . $row["timestamp"]."</td><td>" . $row["r_date"]."</td><td>". $row["comments"]. " </td><td>". $row["location"]."</td><td>" . $row["s_date"]."</td><td>" . $row["e_date"]."</td></tr>";
+        echo "<tr><td>" . $row["iid"]. "</td><td>" . $row["timestamp"]."</td><td>" . $row["r_date"]."</td><td>". $row["comments"]. " </td><td>". $row["location"]."</td><td>" . $row["s_date"]."</td><td>" . $row["e_date"]."</td></tr>";//retrieve attribute data from database into table
     }
     echo "</table>";
 } else {
